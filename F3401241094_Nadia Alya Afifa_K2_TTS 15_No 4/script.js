0a107140a25fe6071694491d00cac70e6f2b29fa
nadiaalyaa <nadiaalya@apps.ipb.ac.id>
2025-12-07 22:20:18 +0700

    Delete F3401241094_Nadia Alya Afifa_K2_TTS 15_No 4 directory

--- F3401241094_Nadia Alya Afifa_K2_TTS 15_No 4/script.js
+++
@@ -1,6 +0,0 @@
-document.getElementById('year').textContent = new Date().getFullYear();
-// Ganti nama & link
-document.getElementById('name').textContent = 'Nadia Alya Afifa';
-document.getElementById('github').href = 'https://github.com/username';
-document.getElementById('linkedin').href = 'https://www.linkedin.com/in/username';
-document.getElementById('email').href = 'mailto:email@domain.com';
